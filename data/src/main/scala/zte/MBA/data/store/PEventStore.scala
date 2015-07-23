56fc1e78e90f6ddc27fa7fa8f8d792585f6410c7
23237187 <uestc2803305011@gmail.com>
2015-07-23 14:17:38 +0800

    2015/07/23 version 2

--- data/src/main/scala/zte/MBA/data/store/PEventStore.scala
+++ data/src/main/scala/zte/MBA/data/store/PEventStore.scala
@@ -3,7 +3,7 @@ package zte.MBA.data.store
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.joda.time.DateTime
-import zte.MBA.data.storage.{Event, Storage}
+import zte.MBA.data.storage.{PropertyMap, Event, Storage}
 
 object PEventStore {
   @transient lazy private val eventsDb = Storage.getPEvents()
@@ -19,6 +19,38 @@ object PEventStore {
           targetEntityType: Option[Option[String]] = None,
           targetEntityId: Option[Option[String]] = None
             )(sc: SparkContext): RDD[Event] = {
-    val (appId, channelId) = Common
+    val (appId, channelId) = Common.appNameToId(appName, channelName)
+
+    eventsDb.find(
+      appId = appId,
+      channelId = channelId,
+      startTime = startTime,
+      untilTime = untilTime,
+      entityType = entityType,
+      entityId = entityId,
+      eventNames = eventNames,
+      targetEntityType = targetEntityType,
+      targetEntityId = targetEntityId
+    )(sc)
+  }
+
+  def aggregateProperties(
+                         appName: String,
+                         entityType: String,
+                         channelName: Option[String] = None,
+                         startTime: Option[DateTime] = None,
+                         untilTime: Option[DateTime] = None,
+                         required: Option[Seq[String]] = None
+                           )(sc: SparkContext): RDD[(String, PropertyMap)] = {
+    val (appId, channelId) = Common.appNameToId(appName, channelName)
+
+    eventsDb.aggregateProperties(
+      appId = appId,
+      entityType = entityType,
+      channelId = channelId,
+      startTime = startTime,
+      untilTime = untilTime,
+      required = required
+    )(sc)
   }
 }
